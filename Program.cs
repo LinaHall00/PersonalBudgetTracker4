a14262b18fb37430ea3bb70fbece4fe74ebf434a
Lina Hall <229102663+LinaHall00@users.noreply.github.com>
2025-10-20 10:58:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalBudgetTracker4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
